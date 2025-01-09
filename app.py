ad5109cd357827ab4fd13c2c2048146005b7ff0d
D-Acho <dianaacho21@gmail.com>
2025-01-08 21:18:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ if disp_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
-<<<<<<< HEAD
     
 # crear una casilla de verificación
 horizontal_bar= st.checkbox('Construir diagrama de barras horizontal')
